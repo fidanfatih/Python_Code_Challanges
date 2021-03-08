3654b687062e4bd9167fa0f1eab6164e4d187606
Kirshoff <fatihfidan52@gmail.com>
2021-03-08 01:31:24 +0100

    up

--- self_study/einstainın çözemediği.py
+++ self_study/einstainın çözemediği.py
@@ -1,7 +1,7 @@
 # Hadi Einstein'a biraz yarsımcı olalım...
 # https://www.matematikciler.com/einsteinin-cozemedigi-soru/
 def eggs():
-    i=100
+    i=0
     while True:
         if not any((i%2,i%3,i%4,i%5,i%6,i%7,i%8,i%9,i%10,(i+1)%11)): break
         i+=10
